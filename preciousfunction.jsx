a6be80c85aa7de0e24425e0c9f7d132ec2009e4d
Sosthene <sosthenemounsambote14@gmail.com>
2024-01-07 14:53:55 +0000

    Update preciousfunction.jsx

--- preciousfunction.jsx
+++ preciousfunction.jsx
@@ -1,26 +1,24 @@
 //handleclickoutside
 
-function useOutsideAlerter(ref,clickref) {
-        useEffect(() => {
-            function handleClickOutside(event) {
-                if (canMaskNotifications) {
-                    if (ref.current && !ref.current.contains(event.target) || clickref.current.contains(event.target)) {
-                        setCanMaskNotifications(false)
-                    }
-                }
-                else if (!canMaskNotifications) {
-                    if (clickref.current.contains(event.target)) {
-                        setCanMaskNotifications(true)
-                    }
-                }
-            }
-            document.addEventListener("mousedown", handleClickOutside);
-            return () => {
-                document.removeEventListener("mousedown", handleClickOutside);
-            };
-        }, [ref, canMaskNotifications]);
-    }
-
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (e.target !== undefined) {
+        if (
+          notificatiion_container.current &&
+          !notificatiion_container.current.contains(e.target) &&
+          e.target.nodeName !== "HTML" &&
+          !icon.current.contains(e.target)
+        ) {
+          setCanMaskNotifications(false);
+          setSeeNewNotifications(false);
+        }
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 //update json file localy
   const hanldeDatas = (name, e) => {
     setData({ ...data, [name]: e.target.value });
